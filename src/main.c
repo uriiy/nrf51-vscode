fbf404ea0bd40956ef6088f6e31535fcc9d479d0
Uriiy <uriiy@tpu.ru>
2021-12-28 23:09:55 +0700

    init

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include "nrf.h"
+#include "nrf_delay.h"
+#include "nrf_gpio.h"
+
+int main(void)
+{
+    nrf_gpio_cfg_output(0);
+
+    while (1)
+    {
+    nrf_gpio_pin_set(0);
+    nrf_delay_ms(50);
+    nrf_gpio_pin_clear(0);
+    nrf_delay_ms(50);
+    }
+      
+}
